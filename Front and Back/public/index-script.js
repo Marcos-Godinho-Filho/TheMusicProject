ae55a5e9e795dbedbd89b6e80e018894d224c8ee
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-10-16 12:35:53 -0300

    arrumei a barra de pesquisa

--- Front and Back/public/index-script.js
+++ Front and Back/public/index-script.js
@@ -93,3 +93,64 @@ function buscar(e) {
         }
     }
 }
+
+var audioplayer = document.getElementById('audioplayer');
+var loaded = false;
+
+var playBtn = document.getElementById('playBtn');
+var pauseBtn = document.getElementById('pauseBtn');
+
+playBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+
+});
+
+
+pauseBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    playBtn.style.display = "inline-block";
+    pauseBtn.style.display = "none ";
+    audioplayer.pause();
+
+});
+
+playSong = (file) => {
+
+    if (loaded == false) {
+        audioplayer.innerHTML = `<source src="` + file + ` " type="audio/mp3" />`;
+        loaded = true;
+    }
+
+    audioPlayer.play();
+
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline-block";
+
+}
+
+document.querySelectorAll('.main-col').forEach(item => {
+
+    item.addEventListener('click', event => {
+
+        let image = item.getAttribute('data-image');
+        let artist = item.getAttribute('data-artist');
+        let song = item.getAttribute('data-song');
+        let file = item.getAttribute('data-file');
+
+        let playerArtistComponent = document.getElementsByClassName("player-artist");
+
+        playerArtistComponent[0].innerHTML = `
+                
+                <img src="` + image + ` "/>
+                <h3>` + song + `<br><span>` + artist + `</span></h3>
+                
+                `;
+
+        playSong(file)
+    });
+
+})
