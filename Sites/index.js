2f08b1e59d275a10e6944d67639de9f9568be284
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-10-18 09:09:53 -0300

    acrescentei um slider que mostra as imagens

--- Sites/index.js
+++ Sites/index.js
@@ -1,6 +1,11 @@
-function mudar(){
-    let img1 = document.getElementById("img1");
-    let img2 = document.getElementById("img2");
-    let img3 = document.getElementById("img3");
-    let img4 = document.getElementById("img4");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    let fadeComplete = function (e) {
+        stage.appendChild(arr[0]);
+    };
+    let stage = document.getElementById("slider");
+    let arr = stage.getElementsByClassName("slider-a");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+}, false);
