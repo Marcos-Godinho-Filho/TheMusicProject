e31df9a7012bbfc500461f0bcdad6ee0af528fec
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-10-17 21:56:01 -0300

    acabei de editar o home por hoje

---
+++ Sites/index.js
@@ -0,0 +1,6 @@
+function mudar(){
+    let img1 = document.getElementById("img1");
+    let img2 = document.getElementById("img2");
+    let img3 = document.getElementById("img3");
+    let img4 = document.getElementById("img4");
+}
