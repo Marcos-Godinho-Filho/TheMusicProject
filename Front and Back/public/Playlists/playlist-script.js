078b0d0a8bafe3ec041070e1bf184c3c650fda8f
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-10-29 22:04:26 -0300

    estilizei as divs

--- Front and Back/public/Playlists/playlist-script.js
+++ Front and Back/public/Playlists/playlist-script.js
@@ -3,49 +3,54 @@ document.querySelector('#play').addEventListener('click', () => {
 })
 
 document.querySelector('#delete').addEventListener('click', () => {
-    document.body.style.filter = 'blur(7px);';
+    document.querySelector('#aside').style.filter = 'blur(7px)';
+    document.querySelector('#header').style.filter = 'blur(7px)';
+    document.querySelector('#main').style.filter = 'blur(7px)';
     const deleteConfirmation = document.querySelector('#deletePlaylistConfirmation');
-    deleteConfirmation.style.fiter = 'none;';
-    deleteConfirmation.className = 'appear';
     deleteConfirmation.innerHTML = `
         <h1> Você tem certeza que deseja deletar a Playlist? </h1>
         <button id="deletePlaylist"> </button>
         <button id="calcelDeletePlaylist"> </button>
     `;
+    deleteConfirmation.style.display = 'block';
 
     document.querySelector('#deletePlaylist').addEventListener('click', () => {
-        document.body.style.filter = 'none;';
+        document.querySelector('#aside').style.filter = 'none';
+        document.querySelector('#header').style.filter = 'none';
+        document.querySelector('#main').style.filter = 'none';
         window.location.href = "../Home/home.html"
         const deleteConfirmation = document.querySelector('#deletePlaylistConfirmation');
-        deleteConfirmation.style.filter = 'none;';
-        deleteConfirmation.className = '';
+        deleteConfirmation.style.display = 'none';
         deleteConfirmation.innerHTML = '';
 
         /* comandos para deletar no banco de dados */
     })
 
     document.querySelector('#cancelDeletePlaylist').addEventListener('click', () => {
-        document.body.style.filter = 'none;';
+        document.querySelector('#aside').style.filter = 'none';
+        document.querySelector('#header').style.filter = 'none';
+        document.querySelector('#main').style.filter = 'none';
         const deleteConfirmation = document.querySelector('#deletePlaylistConfirmation');
-        deleteConfirmation.style.filter = 'none;';
-        deleteConfirmation.className = '';
+        deleteConfirmation.style.display = 'none';
         deleteConfirmation.innerHTML = '';
     })
 })
 
 document.querySelector('#edit').addEventListener('click', () => {
-    document.body.style.filter = 'blur(7px);';
+    document.querySelector('#aside').style.filter = 'blur(7px)';
+    document.querySelector('#header').style.filter = 'blur(7px)';
+    document.querySelector('#main').style.filter = 'blur(7px)';
     const editBox = document.querySelector('#editBox');
-    editBox.style.filter = 'none;';
-    editBox.className = 'appear';
     editBox.innerHTML = `
         <div id="edit-image">
             <img id="edit-img" src="../photos/top50brazil.jpg" alt="Img"> <br>
             <input type="file">
         </div>
         <div id="edit-data">
-            <input type="text" id="title"> Playlist 1 </input> <br>
-            <textarea id="description"> Uma playlist para ouvir quando estiver sozinho </textarea>
-        </div>
-    `;
+            <label> Playlist: </label>
+            <input type="text" id="title"> </input> 
+            <label> Descrição: 
+            <textarea id="description"> </textarea>
+        </div>`;
+    editBox.style.display = 'block';
 })
