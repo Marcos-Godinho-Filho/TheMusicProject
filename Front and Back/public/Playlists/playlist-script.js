590f69fda9184d7ee00abdc641930016587db9a4
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-10-29 22:40:16 -0300

    criei um seletor de imagens na playlist

--- Front and Back/public/Playlists/playlist-script.js
+++ Front and Back/public/Playlists/playlist-script.js
@@ -9,8 +9,8 @@ document.querySelector('#delete').addEventListener('click', () => {
     const deleteConfirmation = document.querySelector('#deletePlaylistConfirmation');
     deleteConfirmation.innerHTML = `
         <h1> Você tem certeza que deseja deletar a Playlist? </h1>
-        <button id="deletePlaylist"> </button>
-        <button id="calcelDeletePlaylist"> </button>
+        <button id="deletePlaylist"> Deletar </button>
+        <button id="calcelDeletePlaylist"> Cancelar </button>
     `;
     deleteConfirmation.style.display = 'block';
 
@@ -42,15 +42,43 @@ document.querySelector('#edit').addEventListener('click', () => {
     document.querySelector('#main').style.filter = 'blur(7px)';
     const editBox = document.querySelector('#editBox');
     editBox.innerHTML = `
+        <h1 id="Editar">
         <div id="edit-image">
             <img id="edit-img" src="../photos/top50brazil.jpg" alt="Img"> <br>
-            <input type="file">
+            <input type="file" value="" id="fileReader" accept="image/*">
         </div>
         <div id="edit-data">
             <label> Playlist: </label>
             <input type="text" id="title"> </input> 
             <label> Descrição: 
             <textarea id="description"> </textarea>
-        </div>`;
+        </div>
+        <button id="editPlaylist"> </button>
+        <button id="calcelEditPlaylist"> </button>
+        `;
     editBox.style.display = 'block';
+
+    const reader = new FileReader();
+    const fileInput = document.querySelector("#fileReader");
+    const img = document.querySelector("#edit-img");
+    reader.onload = e => {
+        img.src = e.target.result;
+        img.style.width = '80%';
+        img.style.height = '80%';
+    }
+    fileInput.addEventListener('change', e => {
+        const f = e.target.files[0];
+        reader.readAsDataURL(f);
+    })
 })
+
+// const reader = new FileReader();
+// const fileInput = document.getElementById("");
+// const img = document.getElementById("img");
+// reader.onload = e => {
+//   img.src = e.target.result;
+// }
+// fileInput.addEventListener('change', e => {
+//   const f = e.target.files[0];
+//   reader.readAsDataURL(f);
+// })
