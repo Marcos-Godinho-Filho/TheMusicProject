746c21c65adcab94c85664b0735bd9391e1c7703
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2022-11-03 11:00:25 -0300

    added everythiing

--- Front and Back/public/index-script.js
+++ Front and Back/public/index-script.js
@@ -1,13 +1,3 @@
-function carregarMenu() {
-    document.querySelector('#menu').style.marginTop = '0vh';
-}
-function voltar() {
-    document.querySelector('#menu').style.marginTop = '-50vh';
-}
-
-document.querySelector('#irparamenu').onclick = carregarMenu();
-document.querySelector('#voltar').onclick = voltar();
-
 let buscarBtn = document.querySelector("#search");
 let inpBx = document.querySelector("#inputBox");
 let results = document.querySelector("#results");
@@ -16,7 +6,7 @@ const baseUrl = 'http://localhost:3000/';
 
 buscarBtn.addEventListener('click', buscar);
 
-inpBx.addEventListener('keyup', function(e){
+inpBx.addEventListener('keyup', function(e) {
     e.preventDefault();
     let tecla = e.keyCode;
     if (tecla === 13)
@@ -24,75 +14,272 @@ inpBx.addEventListener('keyup', function(e){
 })
 
 function buscar(e) {
-    results.innerHTML = 'Resultados da Busca...'
+    results.innerHTML = '';
+    document.querySelector('.container-animation').style.display = 'flex';
 
     e.preventDefault();
     postInfo();
-    getInfo();
+    setTimeout(getInfo, 2000);
 
-    async function postInfo(e) // async --> funciona ao mesmo tempo (assincrono)
-    {
+    async function postInfo(e) {
         if (inpBx.value == "") {
             return
         }
-        const res = await fetch(baseUrl, // espera executar a função para poder continuar
-            {
-                method: 'POST',
-                headers: {
-                    "Content-Type": 'application/json'
-                },
-                body: JSON.stringify({
-                    parcel: inpBx.value
-                })
-            });
+        const res = await fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify({
+                parcel: inpBx.value
+            })
+        });
     }
 
-    setTimeout(getInfo, 2000);
-
     async function getInfo(e) {
-        //e.preventDefault();
-        // alert("Chegou no getInfo()");
-        const res = await fetch(baseUrl + 'info/test?key=busca',
-            {
-                method: 'GET'
-            });
+        const res = await fetch(baseUrl + 'info/test?key=busca', {
+            method: 'GET'
+        });
 
-        console.log(res);
         const data = await res.json();
 
         let resultado = data.info;
 
-        // resultado (retorno) = [{a, b, c}, {a, b, c}]
-
         for (let pos = 0; pos < resultado.length; pos++) {
-            let title = resultado[pos]["title_short"];
-            let artist = resultado[pos]["artist"];
-            let album = resultado[pos]["album"];
-            let duration = resultado[pos]["duration"];
-            let image = resultado[pos]["image"];
-            let preview = resultado[pos]["preview"];
-
             results.innerHTML += `
                 <div class="song">
-                    <div class="items-song" id="s-image">
-                        <img src="${image}" alt="" class="imagem">
+                    <div class="s-image">
+                        <img src="${resultado[pos]["image"]}" alt="" draggable="false">
                     </div>
-                    <div class="items-song" id="s-data">
-                        <h1>${title}</h1>
-                        <h2>${artist}</h2>
-                        <h2>${album}</h2>
+                    <div class="s-data">
+                        <h1>${resultado[pos]["title_short"]}</h1>
+                        <h2>${resultado[pos]["artist"]}</h2>
+                        <h2>${resultado[pos]["album"]}</h2>
                     </div>
-                    <div class="items-song">
-                        <audio controls>
-                            <source id="audio" src=${preview}>
-                            Your browser does not support the audio tag.
-                        </audio>
-                    </div>
-                    <div class="items-song" id="s-time">
-                        <h1>${duration}</h1>
+                    <div class="s-buttons">
+                        <button style="font-size: 20px;" onclick="showSelectPlaylistBox()">
+                            <i class="fa-solid fa-plus" style="color: #fff;"></i>
+                        </button>
+                        <button style="font-size: 20px;" onclick="showSongData('${resultado[pos]["image"]}', '${resultado[pos]["title_short"]}', '${resultado[pos]["artist"]}', '${resultado[pos]["album"]}', '${resultado[pos]["preview"]}');"> 
+                            <i class="fa-solid fa-play" style="color: #fff;"></i>
+                        </button>
                     </div>
                 </div>
                 `;
         }
+        document.querySelector('.container-animation').style.display = 'none';
+        if (results.childNodes.length == 0) {
+            results.innerHTML = '<div id="noResults"> Sem resultados para a busca. </div>'
+        }
     }
+}
+
+document.querySelector('#createPlaylist').addEventListener('click', () => {
+    /* comandos para criar uma nova playlist no BD com nome "Nova Playlist + idAtual", descrição nula e imagem nula, id é identity */
+
+    /* comandos para redirecionar para a nova página, pegar os dados do BD e colocá-los no corpo da playlist */
+})
+
+let image = document.querySelector('#image');
+let title = document.querySelector('#title');
+let artist = document.querySelector('#artist');
+let album = document.querySelector('#album');
+
+let preview = document.querySelector('#preview');
+
+let playBtn = document.querySelector('#playBtn');
+let pauseBtn = document.querySelector('#pauseBtn');
+let backwardBtn = document.querySelector('#backwardBtn');
+let forwardBtn = document.querySelector('#forwardBtn');
+
+let progressBar = document.querySelector('.music-progress-bar')
+let songDuration = document.querySelector('.duration');
+let songCurrentTime = document.querySelector('.current-time');
+
+let volumeSlider = document.querySelector('.volume-slider');
+
+let audioPlayer = document.querySelector('#audioPlayer');
+
+function showSongData(imageSrc, titleTxt, artistTxt, albumTxt, previewSrc) {
+    document.querySelector('.player').style.display = "flex";
+    document.querySelector('#main').style.height = 'calc(100% - 75px - 125px)';
+    document.querySelector('#aside').style.height = 'calc(100% - 125px)';
+
+    document.querySelector('#image').src = imageSrc;
+    document.querySelector('#title').innerHTML = titleTxt;
+    document.querySelector('#artist').innerHTML = artistTxt;
+    document.querySelector('#album').innerHTML = albumTxt;
+    document.querySelector('#preview').src = previewSrc;
+
+    audioPlayer.load();
+
+    playBtn.click();
+
+    progressBar.max = audioPlayer.duration;
+    songDuration.innerHTML = formatTime(29);
+
+    songCurrentTime.innerHTML = '00 : 00';
+}
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0` + min;
+    }
+
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0` + sec;
+    }
+
+    return `${min} : ${sec}`;
+}
+
+playBtn.addEventListener('click', () => {
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+    
+    if (audioPlayer.currentTime >= 29) {
+        backwardBtn.click();
+    }
+    else {
+        audioPlayer.play();
+    }
+})
+
+pauseBtn.addEventListener('click', () => {
+    playBtn.style.display = "inline";
+    pauseBtn.style.display = "none";
+
+    audioPlayer.pause();
+});
+
+backwardBtn.addEventListener('click', () => {
+    audioPlayer.currentTime = 0;
+
+    playBtn.click();
+})
+
+forwardBtn.addEventListener('click', () => {
+    audioPlayer.currentTime = 29;
+})
+
+setInterval(() => {
+    songCurrentTime.innerHTML = formatTime(audioPlayer.currentTime);
+    progressBar.value = audioPlayer.currentTime * 3.4;
+
+    if (audioPlayer.currentTime >= 29) {
+        pauseBtn.click();
+    }
+}, 500)
+
+progressBar.addEventListener('input', () => {
+    audioPlayer.currentTime = progressBar.value / 3.4;
+})
+
+volumeSlider.addEventListener('input', () => {
+    audioPlayer.volume = volumeSlider.value;
+})
+
+let previousVolume = 0;
+let volumeBtn = document.querySelector('#volumeBtn');
+
+volumeBtn.addEventListener('click', () => {
+    let volumeIcon = document.querySelector('#volume-icon');
+
+    if (volumeSlider.value > 0) {
+        previousVolume = volumeSlider.value;
+
+        volumeSlider.value = 0;
+        audioPlayer.volume = 0;
+    } 
+    else if (volumeSlider.value == 0) {
+        volumeSlider.value = previousVolume;
+        audioPlayer.volume = previousVolume;
+    }
+})
+
+let closeSong = document.querySelector('#closeSong');
+
+closeSong.addEventListener('click', () => {
+    audioPlayer.pause();
+    document.querySelector('.player').style.display = "none";
+    document.querySelector('#main').style.height = 'calc(100% - 75px)';
+    document.querySelector('#aside').style.height = '100%';
+})
+
+let showSelectPlaylistBox = function() {
+    let id = '#selectPlaylistBox';
+    let showSelectPlaylistBoxContent = `
+        <h1 class="boxTitle"> Adicionar às playlists </h1>
+        <p> Selecione a playlist desejada: </p>
+        <div id="division"> </div>
+    `; 
+
+    /* Comandos para pegar no banco de dados as playlists do usuário */
+
+    /* Comando de exemplo para mostrar a formatação: */
+    showSelectPlaylistBoxContent += `
+        <div id="playlists">
+            <ul>
+                <li class="playlist" onclick="select(0)">Playlist 1</li>
+                <li class="playlist" onclick="select(1)">Playlist 2</li>
+                <li class="playlist" onclick="select(2)">Playlist 3</li>
+                <li class="playlist" onclick="select(3)">Playlist 4</li>
+                <li class="playlist" onclick="select(4)">Playlist 5</li>
+            <ul>
+        </div>
+    `;
+
+    /* Caso não haja nenhuma playlist criada, mostrar uma instrução ao usuário: */
+    /* deletePlaylistBoxContent += 
+        <p> Você ainda não criou nenhuma playlist. Clique no botão "Criar Playlist" na barra lateral para poder salvar suas músicas favoritas. </p> 
+    `;*/
+        
+    showSelectPlaylistBoxContent += `
+        <div class="options-buttons">
+            <button id="addSongToPlaylist"> Adicionar </button>
+            <button id="calcelDeletePlaylist"> Cancelar </button>
+        </div>
+    `;
+    showBox(id, showSelectPlaylistBoxContent);
+
+    let selectedPlaylist;
+
+    function select(pos) {
+        const playlists = document.getElementsByClassName('playlist');
+
+        selectedPlaylist.style.backgroundColor = 'transparent';
+        
+        selectedPlaylist = playlists[pos];
+        selectedPlaylist.style.backgroundColor = '#505050';
+    }
+
+    document.querySelector('#addSongToPlaylist').addEventListener('click', () => {
+        /* comandos para inserir a música na playlist no BD */
+
+        hideBox(id);
+    })
+
+    document.querySelector('#calcelDeletePlaylist').addEventListener('click', () => {
+        hideBox(id);
+    })
+}
+
+let showBox = function (id, content) {
+    document.querySelector('#aside').style.filter = 'blur(7px)';
+    document.querySelector('#header').style.filter = 'blur(7px)';
+    document.querySelector('#main').style.filter = 'blur(7px)';
+    const box = document.querySelector(id);
+    box.innerHTML = content;
+    box.style.display = 'block';
+}
+
+let hideBox = function (id) {
+    document.querySelector('#aside').style.filter = 'none';
+    document.querySelector('#header').style.filter = 'none';
+    document.querySelector('#main').style.filter = 'none';
+    const box = document.querySelector(id);
+    box.innerHTML = '';
+    box.style.display = 'none';
 }
--- Front and Back/server.js
+++ Front and Back/server.js
@@ -1,8 +1,7 @@
 const axios = require("axios");
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const controller = require('.\\controller.js');
 
 let retorno = [];
 
@@ -12,17 +11,17 @@ app.use(express.json());
 app.get('/info/:dynamic', (req, res) => {
     const { dynamic } = req.params;
     const { key } = req.query;
-    console.log(dynamic, key);
+    // console.log(dynamic, key);
     res.status(200).json({ info: retorno });
     retorno = [];
 });
 
 app.post('/', (req, res) => {
-    
+
     retorno = [];
-    
+
     const { parcel } = req.body;
-    console.log(parcel);
+    // console.log(parcel);
 
     const optionsAxios = {
         method: 'GET',
@@ -34,42 +33,18 @@ app.post('/', (req, res) => {
         }
     };
 
-    axios.request(optionsAxios).then(function (response) {
-        function convertSecToMinutes(sec) {
-            const totalSeconds = sec;
-
-            const minutes = Math.floor(totalSeconds / 60);
-
-            const seconds = totalSeconds % 60;
-
-            function padTo2Digits(num) {
-                return num.toString().padStart(2, '0');
-            }
-
-            const result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
-
-            return result;
-        }
+    axios.request(optionsAxios).then(function(response) {
         for (let musica of response.data['data']) {
-
-            console.log(musica["title_short"]);
-            console.log(musica["artist"]["name"]);
-            console.log(musica["album"]["title"]);
-            console.log(convertSecToMinutes(musica["duration"]));
-            console.log(musica["album"]["cover_medium"]);
-            console.log(musica["preview"]);
-            //retorno = musica["preview"];
             retorno.push({
                 "title_short": musica["title_short"],
                 "artist": musica["artist"]["name"],
                 "album": musica["album"]["title"],
-                "duration": convertSecToMinutes(musica["duration"]),
                 "image": musica["album"]["cover_medium"],
                 "preview": musica["preview"]
             });
 
         }
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.error(error);
     });
     if (!parcel) {
@@ -79,50 +54,4 @@ app.post('/', (req, res) => {
 
 });
 
-
-// Cadastro
-app.post('/registration/', (req, res) => {
-    let parcel = req.body;
-    let nome = parcel[0];
-    let email = parcel[1];
-    let senha = parcel[2];
-
-    try 
-    { 
-        controller.insertNewUser(email, nome, senha);
-        res.redirect("./public/index.html"); // I dont know whether this is working or not
-    }
-    catch (error) { return res.send('Failed to signup'); } // Neither this
-});
-
-
-// Recuperacao de senha
-app.post('/password-registration/', (req, res) => {
-    let parcel = req.body;
-    let email = parcel[0];
-    let novaSenha = parcel[1];
-
-    try
-    {
-        controller.recoverPassword(email, novaSenha);
-        res.statusMessage = 'Password recovery worked out well';
-    }
-    catch (error) { return res.send('Fail in the process'); };
-});
-
-
-// Logar
-app.post('/authentication/', (req, res) => {
-    let parcel = req.body;
-
-    try
-    {
-        if (controller.checkValidation(parcel))
-            res.redirect('./public/index.html');
-        else
-            res.send('User not registered');
-    }
-    catch (error) { return res.send('Failed while trying to login'); }
-});
-
-app.listen(port, () => console.log('Server has started on port ' + port)); 
+app.listen(port, () => console.log('Server has started on port ' + port));
