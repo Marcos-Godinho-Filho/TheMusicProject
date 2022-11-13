2f73ab97c90cc69701a47b67004a2464f926e03d
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2022-11-13 18:27:16 -0300

    quase la

--- Front and Back/public/Profile/profile-script.js
+++ Front and Back/public/Profile/profile-script.js
@@ -15,10 +15,10 @@ document.querySelector('#delete').addEventListener('click', () => {
     let id = '#deleteUserBox';
     let deleteUserBoxContent = `
         <h1 class="boxTitle"> Deletar Usuário </h1>
-        <p class="message"> Tem certeza que deseja deletar o Usuário? (Esta ação é irreversível. Todas as suas playlists e músicas salvas serão perdidas) </p>
-        <div class="password">
-            <p class="message"> Digite sua senha para deletar: </p>
-            <input type="text">
+        <p class="warningMessage"> Esta ação é irreversível. Todas as suas playlists e músicas salvas serão perdidas. </p>
+        <div>
+            <p class="message"> Digite sua senha para confirmar a deleção: </p>
+            <input id="passwordToDeleteUser" type="password">
         </div>
         <div class="options-buttons">
             <button id="deleteUser"> Deletar </button>
@@ -44,9 +44,9 @@ document.querySelector('#edit').addEventListener('click', () => {
     let id = '#editUserBox';
     let editBoxContent = `
         <h1 class="boxTitle"> Editar Usuário </h1>
-        <div class="card-color">
+        <div class="card-color-edit">
             <label for="cor">
-                <input type="color" name="cor" id="cor" class="cor" value="#ff0000">
+                <input type="color" name="cor" id="color" value="#ff0000">
             </label>
         </div> 
         <div id="edit-image">
@@ -56,8 +56,8 @@ document.querySelector('#edit').addEventListener('click', () => {
             <input type="file" id="fileReader" accept="image/*">
         </div>
         <div id="edit-data">
-            <input type="text" id="newUsername" value="User"> </input> 
-            <textarea id="newBio">Bio</textarea>
+            <input type="text" id="newUsername" value="${document.querySelector('#username').innerHTML}"> </input> 
+            <textarea id="newBio" resize="none">${document.querySelector('#bio').innerHTML}</textarea>
         </div>
         <div class="options-buttons">
             <button id="saveEditUser"> Salvar </button>
@@ -66,43 +66,44 @@ document.querySelector('#edit').addEventListener('click', () => {
         `;
     showBox(id, editBoxContent);
 
-    // let previousImg = '';
+    let previousImg = '';
 
-    // const reader = new FileReader();
-    // const fileInput = document.querySelector("#fileReader");
-    // const img = document.querySelector("#img-button");
-    // reader.onload = e => {
-    //     img.src = e.target.result;
-    //     previousImg = img.src;
-    //     img.style.width = '200px';
-    //     img.style.height = '200px';
-    // }
-    // fileInput.addEventListener('change', e => {
-    //     const f = e.target.files[0];
-    //     reader.readAsDataURL(f);
-    // })
+    const reader = new FileReader();
+    const fileInput = document.querySelector("#fileReader");
+    const img = document.querySelector("#img-button");
+    reader.onload = e => {
+        img.src = e.target.result;
+        previousImg = img.src;
+        img.style.width = '120px';
+        img.style.height = '120px';
+    }
+    fileInput.addEventListener('change', e => {
+        const f = e.target.files[0];
+        reader.readAsDataURL(f);
+    })
 
-    // document.querySelector('#edit-img-button').addEventListener('mouseover', () => {
-    //     previousImg = img.src;
-    //     img.src = '../images/browse.png';
-    //     img.style.width = '200px';
-    //     img.style.height = '200px';
-    // })
-    // document.querySelector('#edit-img-button').addEventListener('mouseout', () => {
-    //     img.src = previousImg;
-    //     img.style.width = '200px';
-    //     img.style.height = '200px';
-    // })
+    document.querySelector('#edit-img-button').addEventListener('mouseover', () => {
+        previousImg = img.src;
+        img.src = '../images/browse.png';
+        img.style.width = '120px';
+        img.style.height = '120px';
+    })
+    document.querySelector('#edit-img-button').addEventListener('mouseout', () => {
+        img.src = previousImg;
+        img.style.width = '120px';
+        img.style.height = '120px';
+    })
 
     document.querySelector('#saveEditUser').addEventListener('click', () => {
         /* comandos para salvar a edição no BD */
 
-        document.querySelector('#title').innerHTML = document.querySelector('#newTitle').value
-        document.querySelector('#description').innerHTML = document.querySelector('#newDescription').value;
-        const img = document.querySelector('#t-img');
+        document.querySelector('#username').innerHTML = document.querySelector('#newUsername').value
+        document.querySelector('#bio').innerHTML = document.querySelector('#newBio').value;
+        document.querySelector('.card-color').style.backgroundColor = document.querySelector('#color').value;
+        const img = document.querySelector('#profile-img');
         img.src = document.querySelector('#img-button').src;
-        img.style.width = '220px';
-        img.style.height = '220px';
+        img.style.width = '120px';
+        img.style.height = '120px';
         hideBox(id);
     })
 
